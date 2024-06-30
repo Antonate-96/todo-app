e006de6a7eaae5a6b80cf7128ed02e3273426aa1
natthaphon.a <natthaphon.a@takeitsolution.com>
2024-06-30 16:30:57 +0700

    Config

--- android/app/src/main/kotlin/com/example/todos/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/todos/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.todos
+package com.moozupak.todos
 
 import io.flutter.embedding.android.FlutterActivity
 
---
+++ lib/view/home/home.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Home"),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Home',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
